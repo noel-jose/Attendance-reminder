2451482f9ee657ca4699791668552fbc7748dd2a
noel-jose <noeljosetp@gmail.com>
2021-06-24 21:19:27 +0530

    made the attendance alert into a module

--- index.js
+++ index.js
@@ -6,25 +6,15 @@ prefix = "!";
 
 const client = new Discord.Client();
 
-client.once("ready", () => {
-    console.log("Ready!");
-});
 
 var attendSched = 0;
 var bgmiSchedule = 0;
 const active = new Set();
 const bgmiMessages = ["Bhaa guys kalikkam \n@everyone", "Padutham nirth BGMI il kerr\n@everyone", "Cmon guys chicken dinner adikkam\n@everyone", "@everyone lets kill some bots", "Keri vaa guys\n@everyone", "6:40 aayi punctuality ille ningalk\n@everyone", "BGMI waiting for you guys\n@everyone"];
 
-
-client.on("message", (message) => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
-    const commandName = args.shift().toLowerCase();
-
-    if(commandName === "attendance")
-    {
-        if(attendSched === 0)
+//function to activate/deactivate the attendance alert 
+function attendanceSetter(){
+    if(attendSched === 0)
         {
             message.channel.send("attendance reminder is ON");
             attendSched = crontab.scheduleJob("36 8-12 * * 1-5", function(){ //This will call this function every hour from 8-12 and every day from monday-friday
@@ -39,12 +29,11 @@ client.on("message", (message) => {
             message.channel.send("Attendance reminder is turned OFF");
             active.delete("attendance");
         }
-    }
+}
 
-    //command for bgmi schedule
-    if(commandName === "bgmi")
-    {
-        if(bgmiSchedule === 0)
+//function to activate/deactivate the bgmi alert
+function bgmiSetter(){
+    if(bgmiSchedule === 0)
         {
             message.channel.send("BGMI reminder is ON");
             bgmiSchedule = crontab.scheduleJob("35 18 * * *", function(){ //This will call this function every day at 6:35 PM
@@ -60,6 +49,30 @@ client.on("message", (message) => {
             message.channel.send("BGMI reminder is turned OFF");
             active.delete("bgmi");
         }
+}
+
+client.once("ready", () => {
+    console.log("Ready!");
+    attendanceSetter();
+    bgmiSetter();
+});
+
+
+client.on("message", (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    if(commandName === "attendance")
+    {
+        attendanceSetter();
+    }
+
+    //command for bgmi schedule
+    if(commandName === "bgmi")
+    {
+        bgmiSetter();
     }
     if(commandName === "help")
     {
